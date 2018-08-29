555071ce1866e904f8653d53465a0d882c4a8b23
introvertkernel <gurunathab@gmail.com>
2018-08-29 16:44:05 +0530

    Ok

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ session_start();
 $_SESSION['message'] = '';
 $mysqli = new mysqli('localhost','','','EmpData');
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $username = $mysqli->real_escape_string($_POST[])
+    $emp_name = $mysqli->real_escape_string($_POST['emp_name']);
+    $emp_designation = $mysqli->real_escape_string($_POST['emp_designation'])
+    $report_manager = $mysqli->real_escape_string($_POST['report_manager'])
 }
 ?>

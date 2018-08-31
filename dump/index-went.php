e501d23efbe8bade90c7e294f278d87a618cb703
introvertkernel <gurunathab@gmail.com>
2018-08-31 17:24:30 +0530

    DONE

--- dump/index-went.php
+++ dump/index-went.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
-<html>
- <head>
-  <title>Webslesson Tutorial | Autocomplete Textbox using Bootstrap Typehead with Ajax PHP</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />  
- </head>
- <body>
+<html lang="en">
 
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OrgChart</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="css/main.css"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="js/bootstrap3-typeahead.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <a href="index.php" class="navbar-brand text-center">OrgChart</a>
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
             <span class="navbar-toggler-icon"></span>
@@ -27,7 +35,8 @@
             </div>
         </div>
     </nav>
-
+    
+       
     <div class="container-fluid h-100">
         <!-- <div class="w-100"></div>  comments -->
         <div class="row row-full">
@@ -85,11 +94,39 @@
         </div>
     
     </div>
+</body>
+
+</html>
+
+<!-- <script>
+$(document).ready(function(){  
+  $('#report_manager').keyup(function(){  
+        var query = $(this).val();  
+        if(query != '')  
+        {  
+            $.ajax({
+                  url:"find.php",  
+                  method:"POST",  
+                  data:{query:query},  
+                  success:function(data)  
+                  {  
+                      $('#managerList').fadeIn();  
+                      $('#managerList').html(data);  
+                  }
+            });  
+        }
+  });  
+  $(document).on('click', 'li', function(){  
+        $('#report_manager').val($(this).text());  
+        $('#managerList').fadeOut();  
+  });  
+});
+</script> -->
 
 <script>
 $(document).ready(function(){
  
- $('#report_manager').typeahead({
+ $('#country').typeahead({
   source: function(query, result)
   {
    $.ajax({
---
+++ get.php
@@ -0,0 +1,22 @@
+<?php
+//fetch.php
+$connect = mysqli_connect("localhost", "root", "", "EmpData");
+$request = mysqli_real_escape_string($connect, $_POST["query"]);
+$query = "
+ SELECT * FROM Employee WHERE emp_name LIKE '%".$request."%'
+";
+
+$result = mysqli_query($connect, $query);
+
+$data = array();
+
+if(mysqli_num_rows($result) > 0)
+{
+ while($row = mysqli_fetch_assoc($result))
+ {
+  $data[] = $row["emp_name"];
+ }
+ echo json_encode($data);
+}
+
+?>

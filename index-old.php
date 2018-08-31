53e12b2cc34a69c543e398b38cccd2f357cf63a8
introvertkernel <gurunathab@gmail.com>
2018-08-31 11:21:09 +0530

    saveme

---
+++ index-old.php
@@ -0,0 +1,100 @@
+</html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>OrgChart</title>
+  <!-- <link rel="stylesheet" href="css/main.css"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+
+</head>
+
+<body>
+  <header>
+    <div id="navbar">
+      <h1>OrgChart</h1>
+      <nav>
+        <ul>
+          <li><a href="index.php">Form</a></li>
+          <li><a href="view.php">View</a></li>
+        </ul>
+      </nav>
+
+    </div>
+  </header>
+
+  <link rel="main" href="main.css" type="text/css">
+  <div class="body-content">
+    <div class="module">
+      <h1>Enter Employee details
+      </h1>
+      <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
+        <div class="alert alert-error"></div>
+        <input type="text" placeholder="Employee Name" name="emp_name" required />
+        <input type="text" placeholder="Employee Designation" name="emp_designation" required />
+        <input type="text" placeholder="Reporting Manager" name="report_manager" id="report_manager" class="container form-control input-lg" autocomplete="off" required />
+        <div id="managerList" class="form-control input-lg"> </div>
+        <input type="submit" value="Submit" name="submit" class="btn" />
+      </form>
+    </div>
+  </div>
+
+
+
+</body>
+
+</html>
+
+
+<script>  
+
+
+// $(document).ready(function(){
+ 
+//  $('#report_manager').typeahead({
+//   source: function(query, result)
+//   {
+//    $.ajax({
+//     url:"find.php",
+//     method:"POST",
+//     data:{query:query},
+//     dataType:"json",
+//     success:function(data)
+//     {
+//      result($.map(data, function(item){
+//       return item;
+//      }));
+//     }
+//    })
+//   }
+//  });
+ 
+// });
+
+
+$(document).ready(function(){  
+  $('#report_manager').keyup(function(){  
+        var query = $(this).val();  
+        if(query != '')  
+        {  
+            $.ajax({
+                  url:"find.php",  
+                  method:"POST",  
+                  data:{query:query},  
+                  success:function(data)  
+                  {  
+                      $('#managerList').fadeIn();  
+                      $('#managerList').html(data);  
+                  }
+            });  
+        }
+  });  
+  $(document).on('click', 'li', function(){  
+        $('#report_manager').val($(this).text());  
+        $('#managerList').fadeOut();  
+  });  
+});  
+</script>
--- index.php
+++ index.php
@@ -1,81 +1,182 @@
-</html>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <title>OrgChart</title>
-  <link rel="stylesheet" href="main.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />   -->
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OrgChart</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="css/main.css"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
-<body>
-  <header>
-    <div id="navbar">
-      <h1>OrgChart</h1>
-      <nav>
-        <ul>
-          <li><a href="index.php">Form</a></li>
-          <li><a href="view.php">View</a></li>
-        </ul>
-      </nav>
+<body><div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a href="#" class="navbar-brand text-center mx-auto">OrgChart</a>
+        <!-- <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarMenu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">View</a>
+                </li>
+            </ul>
+        </div> -->
 
+
+    
+    </nav></div>
+    
+        <!-- <div class="container">
+        <div class="row equal">
+            <div class="col-md-6 col-sm-6">
+                <div class="footer-widget">
+                    <br>
+                    <p>lorem ipsum</p>
+                    <p>lorem ipsum</p>
+                    <p>lorem ipsum</p>          
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <div class="footer-widget">
+                    <p>lorem ipsum</p>
+                    <p>lorem ipsum</p>
+                    <p>lorem ipsum</p>          
+                </div>
+            </div>
+        </div>
+    </div> -->
+    
+    
+    
+    
+    
+    <!--  -->
+    <!-- <div class="container-fluid h-100">
+    <div class="row h-100">
+        <div class="col-sm-6 invisible col-2">hidden spacer</div> -->
+            <!-- <div class="col rightside offset-2 offset-sm-6 py-2">
+                <h4>View</h4>
+                <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                    raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
+                    Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+
+                <p>Ethical Kickstarter PBR asymmetrical lo-fi. oreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb
+                    readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies,
+                    forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+                    
+                <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                    raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
+                    Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+
+            </div>
+        </div>
     </div>
-  </header>
+    <div class="row h-100"> -->
+        <!-- <div class="col-sm-6 invisible col-2">hidden spacer</div> -->
+            <!-- <div class="col leftside offset-2 offset-sm-6 py-1">
+                <h4>View</h4>
+                <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                    raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
+                    Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
 
-  <link rel="main" href="main.css" type="text/css">
-  <div class="body-content">
-    <div class="module">
-      <h1>Enter Employee details
-      </h1>
-      <form class="form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
-        <div class="alert alert-error"></div>
-        <input type="text" placeholder="Employee Name" name="emp_name" required />
-        <input type="text" placeholder="Employee Designation" name="emp_designation" required />
-        <input type="text" placeholder="Reporting Manager" name="report_manager" id="report_manager" class="container form-control input-lg" autocomplete="off" required />
-        <div id="managerList" class="form-control input-lg"> </div>
-        <input type="submit" value="Submit" name="submit" class="btn" />
-      </form>
+                <p>Ethical Kickstarter PBR asymmetrical lo-fi. oreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table Williamsburg slow-carb
+                    readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies,
+                    forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+                    
+                <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote bag. Tote bag cronut semiotics,
+                    raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan cliche semiotics ugh synth chillwave meditation.
+                    Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+
+            </div>
+        </div>
+    </div> -->
+    <!--  -->
+    <div class="container-fluid h-100">
+        <!-- <div class="w-100"></div>  comments -->
+        <div class="row row-full">
+            <div class="col-sm-6 col-md-6 pull-right">
+                <div class="leftside">
+                    <br>
+                    <h2 class="offset-2 ">Form</h2>
+                    <br>
+                    <h4 class="offset-2">Enter Employee details
+                    </h4>
+                    <!-- <form class="form-group main-form needs-validation" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
+                        <div class="alert alert-error"></div>
+                        <input type="text" placeholder="Employee Name" name="emp_name" required />
+                        <input type="text" placeholder="Employee Designation" name="emp_designation" required />
+                        <input type="text" placeholder="Reporting Manager" name="report_manager" id="report_manager" class="container form-control input-lg" autocomplete="off" required />
+                        <div id="managerList" class="form-control input-lg"> </div>
+                        <input type="submit" value="Submit" name="submit" class="btn" /> -->
+
+                    <form class="main-form" action="form.php" method="post" enctype="multipart/form-data" autocomplete="off">
+                        <div class="row text-center alert alert-error       ">
+                            <div class="col-sm-8">
+                                <div class="form-group">
+                                    <div class="alert alert-error"></div>
+                                    <input type="text" placeholder="Employee Name" name="emp_name" required />
+                                    
+                                </div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="form-group">
+                                    <input type="text" placeholder="Employee Designation" name="emp_designation" required />
+                                </div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="form-group">
+                                    <input type="text" placeholder="Reporting Manager" name="report_manager" id="report_manager" autocomplete="off" required />
+                                </div>
+                            </div>
+                            <div class="col-sm-8">
+                                <div class="form-group">
+                                    <div id="managerList" clas="form-control input-lg"> </div>
+                                    <br>
+                                </div>
+                            </div>
+                            <div class="col-sm-8 leftside">
+                                <div class="form-group">
+                                    <input type="submit" value="Submit" name="submit" class="btn" />
+                                </div>
+                            </div>
+                        </div>    
+                    </form>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-6 pull-right">
+                <div class="rightside">
+                    <br>
+                    <h2 class="offset-2">View</h2>
+                    <!-- <div class="container">
+                            <a class="btn btn-primary" href="table.php" role="button">Link</a>
+                            <a class="btn btn-primary" href="list.php" role="button">Link</a>
+                    </div> -->
+                    <br><br>
+                    <div class="offset-2">
+                        <button type="button" class="btn btn-outline-primary btn-lg">Large button</button>
+                    </div>
+
+                </div>
+          
+            </div>
+        </div>
+    
     </div>
-  </div>
-
-
-
 </body>
 
 </html>
 
-
-<script>  
-
-
-// $(document).ready(function(){
- 
-//  $('#report_manager').typeahead({
-//   source: function(query, result)
-//   {
-//    $.ajax({
-//     url:"find.php",
-//     method:"POST",
-//     data:{query:query},
-//     dataType:"json",
-//     success:function(data)
-//     {
-//      result($.map(data, function(item){
-//       return item;
-//      }));
-//     }
-//    })
-//   }
-//  });
- 
-// });
-
-
+<script>
 $(document).ready(function(){  
   $('#report_manager').keyup(function(){  
         var query = $(this).val();  
@@ -97,5 +198,5 @@ $(document).ready(function(){
         $('#report_manager').val($(this).text());  
         $('#managerList').fadeOut();  
   });  
-});  
+});
 </script>
